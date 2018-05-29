6e74495ad8f37c1bc19c89662876c5ff99114d37
natsumi.hironaka <natsumi.hironaka@rakuten.com>
2018-05-29 05:17:10 +0000

    v2

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
 
 <h1>タスク新規登録ページ</h1>
 
-<div class="col-xs-12　col-sm-offset-2 col-sm-8 col-md-offset-2 col-sm-8 col-lg-offset-3 col-sm-6">
+<div class="col-xs-12　col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
